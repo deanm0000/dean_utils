341eefabd876dc1ae6a2dcef1292e96b036364b8
Dean MacGregor <powertrading121@gmail.com>
2024-07-30 12:30:26 -0400

    fix_root_import

--- src/dean_utils/__init__.py
+++ src/dean_utils/__init__.py
@@ -1,7 +1,9 @@
 __all__ = [
     "async_abfs",
     "cos_query_all",
-    "send_to_queue",
+    "send_message",
+    "update_queue",
+    "delete_message",
     "send_email",
     "az_send",
     "def_cos",
@@ -9,6 +11,13 @@ __all__ = [
     "pl_scan_pq",
     "pl_write_pq",
 ]
-from dean_utils.utils.az_utils import async_abfs, cos_query_all, send_to_queue, def_cos
+from dean_utils.utils.az_utils import (
+    async_abfs,
+    cos_query_all,
+    send_message,
+    update_queue,
+    delete_message,
+    def_cos,
+)
 from dean_utils.utils.email_utility import send_email, az_send
 from dean_utils.polars_extras import pl_scan_hive, pl_scan_pq, pl_write_pq
