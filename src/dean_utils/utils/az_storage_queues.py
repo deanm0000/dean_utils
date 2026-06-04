6e904963ef568183c1d7040b40bef3dfeb963a62
Dean MacGregor <powertrading121@gmail.com>
2026-06-04 10:15:19 -0400

    revamp Queue

--- src/dean_utils/utils/az_storage_queues.py
+++ src/dean_utils/utils/az_storage_queues.py
@@ -5,6 +5,7 @@ import json
 import os
 from typing import (
     TYPE_CHECKING,
+    Any,
     overload,
 )
 
@@ -18,14 +19,14 @@ if TYPE_CHECKING:
 
 
 class Queue:
-    def __init__(self, conn_str: None | str = None):
+    def __init__(self, *, conn_str: None | str = None, queue: str | None = None):
         try:
             from azure.storage.queue.aio import QueueServiceClient as QSC
 
         except ImportError as e:
             msg = "azure-storage-queue is not installed, run `pip install azure-storage-queue` to use this functionality"
             raise ImportError(msg) from e
-
+        self.queue = queue
         if conn_str is None:
             self.conn_str = conn_str or os.environ.get("AzureWebJobsStorage", None)
         if self.conn_str is None:
@@ -34,19 +35,34 @@ class Queue:
 
         self.AIO_SERVE = QSC.from_connection_string(conn_str=self.conn_str)
 
+    def change_queue(self, queue: str):
+        self.queue = queue
+
+    def _validate(self, queue: str | None) -> str:
+        if queue is None and self.queue is None:
+            msg = "queue name must be provided either at the class level or the function level"
+            raise ValueError(msg)
+        else:
+            queue = queue or self.queue
+        assert queue is not None
+        return queue
+
     async def peek_messages(
-        self, queue: str, max_messages: int | None = None, **kwargs
+        self, queue: str | None = None, *, max_messages: int | None = None, **kwargs
     ):
         from azure.storage.queue import TextBase64EncodePolicy
+
+        queue = self._validate(queue)
 
         async with self.AIO_SERVE.get_queue_client(
             queue, message_encode_policy=TextBase64EncodePolicy()
         ) as aio_client:
             return await aio_client.peek_messages(max_messages=max_messages, **kwargs)
 
-    async def get_queue_properties(self, queue: str, **kwargs):
+    async def get_queue_properties(self, queue: str | None = None, **kwargs):
         from azure.storage.queue import TextBase64EncodePolicy
 
+        queue = self._validate(queue)
         async with self.AIO_SERVE.get_queue_client(
             queue, message_encode_policy=TextBase64EncodePolicy()
         ) as aio_client:
@@ -55,9 +71,9 @@ class Queue:
     @overload
     async def send_message(
         self,
-        queue: str,
         messages: list[str],
         *,
+        queue: str | None,
         visibility_timeout: int | None = None,
         time_to_live: int | None = None,
         timeout: int | None = None,
@@ -66,19 +82,20 @@ class Queue:
     @overload
     async def send_message(
         self,
-        queue: str,
         messages: str | dict,
         *,
+        queue: str | None,
         visibility_timeout: int | None = None,
         time_to_live: int | None = None,
         timeout: int | None = None,
         **kwargs,
     ) -> QueueMessage: ...
+
     async def send_message(
         self,
-        queue: str,
-        messages: list[str] | str | dict,
+        messages: list[str] | str | dict | None,
         *,
+        queue: str | None = None,
         visibility_timeout: int | None = None,
         time_to_live: int | None = None,
         timeout: int | None = None,
@@ -86,6 +103,7 @@ class Queue:
     ):
         from azure.storage.queue import TextBase64EncodePolicy
 
+        queue = self._validate(queue)
         async with self.AIO_SERVE.get_queue_client(
             queue, message_encode_policy=TextBase64EncodePolicy()
         ) as aio_client:
@@ -118,17 +136,18 @@ class Queue:
 
     async def update_queue(
         self,
-        queue,
-        id,
-        pop_receipt,
-        message,
+        id: str,
+        pop_receipt: str,
+        message: str | dict,
         *,
+        queue: str | None = None,
         visibility_timeout: int | None = None,
         timeout: int | None = None,
         **kwargs,
     ):
         from azure.storage.queue import TextBase64EncodePolicy
 
+        queue = self._validate(queue)
         async with self.AIO_SERVE.get_queue_client(
             queue, message_encode_policy=TextBase64EncodePolicy()
         ) as aio_client:
@@ -144,13 +163,15 @@ class Queue:
 
     async def delete_message(
         self,
-        queue,
-        id,
-        pop_receipt,
+        id: str,
+        pop_receipt: str,
+        *,
+        queue: str | None = None,
         **kwargs,
     ):
         from azure.storage.queue import TextBase64EncodePolicy
 
+        queue = self._validate(queue)
         async with self.AIO_SERVE.get_queue_client(
             queue, message_encode_policy=TextBase64EncodePolicy()
         ) as aio_client:
@@ -163,18 +184,25 @@ class Queue:
 
     async def clear_messages(
         self,
-        queue,
+        queue: str | None = None,
         **kwargs,
     ):
         from azure.storage.queue import TextBase64EncodePolicy
 
+        queue = self._validate(queue)
         async with self.AIO_SERVE.get_queue_client(
             queue, message_encode_policy=TextBase64EncodePolicy()
         ) as aio_client:
             task = await aio_client.clear_messages(**kwargs)
             return task
 
-    async def retry(self, queue: str, message: str | dict, visibility_timeout: int):
+    async def retry(
+        self,
+        message: str | dict,
+        visibility_timeout: int = 600,
+        *,
+        queue: str | None = None,
+    ):
         """
         Async Context handler that creates a queue message on open and then deletes it on close.
 
@@ -192,6 +220,10 @@ class Queue:
         async with queue.retry(name_of_queue, message_to_queue, visibility_timeout=timeout):
             # commands
         """
+        if visibility_timeout <= 0:
+            msg = "visibility_timeout must be a positive integer representing seconds until the message becomes visible"
+            raise ValueError(msg)
+        queue = self._validate(queue)
         return QueueRetry(self, queue, message, visibility_timeout)
 
 
@@ -211,8 +243,8 @@ class QueueRetry:
     async def __aenter__(self):
         self.queue_message = asyncio.create_task(
             self.queue.send_message(
-                self.queue_name,
                 self.message,
+                queue=self.queue_name,
                 visibility_timeout=self.visibility_timeout,
             )
         )
@@ -220,7 +252,7 @@ class QueueRetry:
     async def __aexit__(self, exc_type, exc_value, traceback):
         queue_message = await self.queue_message
         await self.queue.delete_message(
-            self.queue_name,
-            queue_message["id"],
-            queue_message["pop_receipt"],
+            queue=self.queue_name,
+            id=queue_message["id"],
+            pop_receipt=queue_message["pop_receipt"],
         )
